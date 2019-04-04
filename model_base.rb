45eb498732ab5923482c5a7ad501e7995ba20a7d
Rupa Dhillon <rupa211984@gmail.com>
2019-04-03 17:47:42 -0700

    Get all table data

--- model_base.rb
+++ model_base.rb
@@ -25,4 +25,19 @@ class ModelBase
 
     new(data.first)
   end
+
+  def self.all
+    table = C_TO_TABLE[name]
+
+    data = QuestionsDatabase.instance.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table}
+    SQL
+
+    return nil if data.empty?
+
+    data.map { |datum| new(datum) }
+  end
 end
